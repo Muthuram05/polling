3d39406ca972906e7d8705d47d85de87db4d442d
magesh <48789795+mageshkumar1598@users.noreply.github.com>
2024-04-24 16:50:48 +0530

    merge commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import './App.css';
-import LoginForm from './components/login';
-import {useAuthState} from "react-firebase-hooks/auth"
+import "./App.css";
+import LoginForm from "./components/login";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./services/firebase";
-import { userStore } from './store';
-import { useEffect } from 'react';
-import { Dashboard } from './components/Dashboard';
+import { userStore } from "./store";
+import { useEffect } from "react";
+import { Dashboard } from "./components/Dashboard";
 
 function App() {
   const [user] = useAuthState(auth);
-  const setUser = userStore((state)=>state.setUser)
-  useEffect(()=>{
-
+  const setUser = userStore((state) => state.setUser);
+  useEffect(() => {
     console.log(user);
-    setUser(user)
-  })
-  return (
-    <div className="App">
-      {user ? <Dashboard /> : <LoginForm />}
-        
-    </div>
-  );
+    setUser(user);
+  });
+  return <div className="App">{user ? <Dashboard /> : <LoginForm />}</div>;
 }
 
 export default App;
