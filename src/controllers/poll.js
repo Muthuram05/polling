fb65ff33e3dff1d0d83b859711736afef458f7fb
Dharani-perumal <dharanivp22@gmail.com>
2024-04-24 20:07:22 +0530

    dummy

--- src/controllers/poll.js
+++ src/controllers/poll.js
@@ -1,7 +1,16 @@
-import { doc, getDoc, setDoc, updateDoc, collection, query, where, getDocs, } from "firebase/firestore";
+import {
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "../services/firebase";
 
-export async function getPoll(authorId){
+export async function getPoll(authorId) {
   const q = query(collection(db, "poll"), where("author", "==", authorId));
 
   const querySnapshot = await getDocs(q);
@@ -12,20 +21,35 @@ export async function getPoll(authorId){
   return polls;
 }
 
-export async function createPoll(documentId, data){
-    const docRef = doc(db, "poll", documentId);
+export async function createPoll(documentId, data) {
+  const docRef = doc(db, "poll", documentId);
 
-    // Check if the document already exists
-    const docSnapshot = await getDoc(docRef);
-    const documentExists = docSnapshot.exists();
+  // Check if the document already exists
+  const docSnapshot = await getDoc(docRef);
+  const documentExists = docSnapshot.exists();
 
-    if (documentExists) {
-      // Document exists, update it
-      await updateDoc(docRef, data);
-      console.log("Document updated successfully!");
-    } else {
-      // Document does not exist, add it
-      await setDoc(docRef, data);
-      console.log("Document added successfully!");
-    }
+  if (documentExists) {
+    // Document exists, update it
+    await updateDoc(docRef, data);
+    console.log("Document updated successfully!");
+  } else {
+    // Document does not exist, add it
+    await setDoc(docRef, data);
+    console.log("Document added successfully!");
+  }
+}
+
+export async function getSpecificPoll(id) {
+  const q = query(collection(db, "poll"), where("id", "==", id));
+  const querySnapshot = await getDocs(q);
+
+  // Check if a document matching the specified id was found
+  if (!querySnapshot.empty) {
+    // There should be only one document with the specified id, so we return the first one
+    const doc = querySnapshot.docs[0];
+    return { id: doc.id, ...doc.data() };
+  } else {
+    // No document found with the specified id
+    return null;
+  }
 }
